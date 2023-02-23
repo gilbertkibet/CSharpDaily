79be22b08f1374c371a4d5a946db240c72c4ed40
Gilbert kibet <kibetgilly354@gmail.com>
2023-02-23 15:33:33 -0800

    Add project files.

---
+++ CSharpDaily/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpDaily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //DEFAULT VALUES IN C#
+
+           // int x = default;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataTypes
+{
+
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+            //int x = default;
+            //Console.WriteLine(x);
+            char[] chars = { 'h', 'e', 'l', 'l', 'o' };
+
+            string  stri1=new string (chars); 
+            //Console.WriteLine(stri1);
+            foreach(char c in stri1)
+            {
+                //Console.WriteLine (c);
+            }
+
+            string message = "This is \"hello\" world";
+            Console.WriteLine (message);
+
+
+        }
+    }
+}
