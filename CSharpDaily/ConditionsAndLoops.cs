4764bc133357ae26f2e67fe163baa2a644da3316
Gilbert kibet <kibetgilly354@gmail.com>
2023-02-24 08:44:56 -0800

    ElseStatementTerniaryOperatoand SwitchStatement

---
+++ CSharpDaily/ConditionsAndLoops.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDaily
+{
+    public class ConditionsAndLoops
+    {
+      public  int i = 10;
+      public   int j = 10; 
+        //if statement
+
+        public ConditionsAndLoops(int i,int j) 
+        { 
+            this.i = i;
+          this.j = j;
+        } 
+
+   //     if (i < j)
+	  //{
+   // //execute code here....conditional expression should return a boolean else it will be a compile time errorr
+	  //}
+
+
+        //multiples else if can be use after if  one can be executed not both
+
+        
+        
+    }
+}
--- CSharpDaily/Program.cs
+++ CSharpDaily/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpDaily
 {
@@ -9,8 +10,73 @@ namespace CSharpDaily
             //Console.WriteLine("Hello World!");
             //DEFAULT VALUES IN C#
 
-           // int x = default;
-            Console.WriteLine(x);
+            // int x = default;
+            int i = 10, j = 20;
+
+            if (i < j)
+
+            {
+                //Console.WriteLine("this is true");
+            }
+
+            //YOU CAN CALL A METHOD THAT RETURN A BOOLEAN INSIDE AN IF STATEMENT
+
+
+
+            //static bool IsGreater(int x, int y)
+            //{
+            //    return x > y;
+            //}
+
+            //if(IsGreater(10, 20))
+            //{
+            //    Console.WriteLine("THIS IS TRUE");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("THIS IS FALSE");
+            //}
+            //if (i == j)
+            //{
+
+            //    Console.WriteLine("i is equal to j");
+
+            //}
+
+            //else if (
+            //    i > j)
+            //{
+            //    Console.WriteLine("I IS GREATER THAN J");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("I IS LESS THAN J");
+            //}
+
+            //terniary operator
+            //condition?statetement1:statement2;  ?: ?:  start with boolena condition if true execute first statement
+            //otherwise second 
+
+            //you declare a variable
+
+            //var outPut = i < j ? "i is less than j" : "i is greater than j";
+
+            //Console.WriteLine(outPut);
+            
+            switch (i)
+            {
+                case 0:
+                    Console.WriteLine("IS IS ZERO");
+                    break;
+                case 10:
+                    Console.WriteLine("The value of i is 10");
+                    break;
+                default:
+                    Console.WriteLine("This is unknown values");
+                    break;
+            }
+
         }
     }
 }
