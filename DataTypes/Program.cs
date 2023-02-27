417fc9b98cd2296a0cd329e4c6b40f6b60d63573
Gilbert kibet <kibetgilly354@gmail.com>
2023-02-27 08:11:52 -0800

    Learning And Understanding Datatypes in C#

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -7,22 +7,41 @@ namespace DataTypes
     {
         static void Main(string[] args) {
 
-
-            //int x = default;
-            //Console.WriteLine(x);
-            char[] chars = { 'h', 'e', 'l', 'l', 'o' };
-
-            string  stri1=new string (chars); 
-            //Console.WriteLine(stri1);
-            foreach(char c in stri1)
-            {
-                //Console.WriteLine (c);
-            }
-
-            string message = "This is \"hello\" world";
-            Console.WriteLine (message);
+            //DAY ONE DATATYEPES IN C#
+            string stringVar = "This is a string ";
+            int intVar = 10;
+            float floatVar = 10.2f;  // we must add suffix f to float otherwise it will be a decimal
+            char charVar='A';//array of character can be used to create strings in c#
+            bool boolVar = false;
+            DateTime dateTimeVar = DateTime.Now;
 
 
+            //DEFAULT VALUES IN C#
+            int varint = default; //zero
+            bool isMale = default; //zero
+            decimal dec=default;//zero
+            float f=default; //zero
+
+            Console.WriteLine(isMale);
+
+            //REFERENCES TYPES BY DEFAULT ARE STRING AND OBJECT
+            String si = "Hello";
+            string s2 = "World";
+
+            //when a variable of value type is converted to object it is called boxing
+            //when a variable of type object is converted to value type it is called unboxing
+
+            //
+            string A = "Geeks";
+            A += "for";
+            A += "Geeks";
+            //obj
+            object obj = 20;
+            Console.WriteLine(obj.GetType());
+
+            //SHOULD LEARN ABOUT pointer datatypes(&AND *)
+            int n = 10;
+           // int* p = &n;
         }
     }
 }
